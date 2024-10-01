153e740b86bb7e5e31bbc94dd180aeebe1ead49b
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-01 10:55:17 +0530

    Create Q7.py

---
+++ Q7.py
@@ -0,0 +1,38 @@
+'''
+You are given principal amount, rate of interest per annum, and time in years. You need to calculate the simple interest.
+Input and Output Format
+Input Format:
+The first line contains the principal amount (principal).
+The second line contains the rate of interest (rate) per annum.
+The third line contains the time (time) in years.
+Output Format:
+A single line containing the simple interest calculated.
+Sample Input 1
+1000
+5
+2
+Sample Output 1
+100.0
+Sample Input 2
+5000
+8.5
+3
+Sample Output 2
+1275.0
+Sample Input 3
+5000
+8.5
+3
+Sample Output 3
+525.0
+'''
+def calculate_simple_interest(principal, rate, time):
+    # Simple Interest formula: SI = (Principal * Rate * Time) / 100
+    simple_interest = (principal * rate * time) / 100
+    return simple_interest
+if __name__ == "__main__":
+    principal = float(input("Enter the principal amount:\n").strip())
+    rate = float(input("Enter the rate of interest per annum:\n").strip())
+    time = float(input("Enter the time in years:\n").strip())
+    interest = calculate_simple_interest(principal, rate, time)
+    print(interest)
